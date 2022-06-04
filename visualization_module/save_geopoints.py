449968a7225510187ad3f35b4cc1a537d1be9143
ValentinaFedorova <57736319+ValentinaFedorova@users.noreply.github.com>
2022-06-04 15:39:26 +0800

    Update save_geopoints.py

--- visualization_module/save_geopoints.py
+++ visualization_module/save_geopoints.py
@@ -2,9 +2,9 @@ import pyodbc
 import pandas as pd
 import json
 
-geopoint_path = 'D:\\magistrValya\\address_search_system\\visualization_module\\geopoints.json'
+geopoint_path = 'my_path'
 
-cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-2ELPTI6;DATABASE=prom')
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=MY_SERVER;DATABASE=prom')
 cursor = cnxn.cursor()
 addresses = pd.read_sql("SELECT a.[URL_ID], l.url_addr, a.[ADDRESS_RES], a.[GEOPOINT].Lat as geo_lat, a.[GEOPOINT].Long as geo_lon FROM [OARB].[ADDRESS] (nolock) a inner join [OARB].[LINKS] (nolock) l on a.URL_ID = l.ID where a.[PROCESSED] = 3",cnxn)
 geopoints_res = []
